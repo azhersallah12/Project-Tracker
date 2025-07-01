fe0a1aca0f446cc56d65f207704474190e7102f8
azhersallah12 <azheradobe@gmail.com>
2025-07-01 19:10:48 +0300

    Update firebase-config.js

--- .gitignore/firebase-config.js
+++ .gitignore/firebase-config.js
@@ -1,1 +1,15 @@
 
+// IMPORTANT: You DO NOT create this file yourself.
+// This file is created automatically by the deploy.yml script during deployment.
+// The values below are filled in from your GitHub repository secrets.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD5OsLRbBc8l5PXWw9vozwdye96FarFDIE",
+  authDomain: "project-tracker-22bc4.firebaseapp.com",
+  databaseURL: "https://project-tracker-22bc4-default-rtdb.firebaseio.com",
+  projectId: "project-tracker-22bc4",
+  storageBucket: "project-tracker-22bc4.appspot.com",
+  messagingSenderId: "147357551717",
+  appId: "1:147357551717:web:472f8dca4d1ab67085ca8c",
+  measurementId: "G-5TKFL1SY7D"
+};
